85ed55cbbb19af08d855fa642eae1e901d460677
svetanto <svetanto@gmail.com>
2017-05-09 00:52:02 +0300

    Выполнил восьмое задание

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 var map_open = document.querySelector(".contacts img");
 var bigmap = document.querySelector(".map-popup-structure");
 var map_close = bigmap.querySelector(".close-button");
-map_open.addEventListener("click", function(evt){
+map_open.addEventListener("click", function(evt) {
   evt.preventDefault();
   bigmap.classList.add("popup-show");
 });
-map_close.addEventListener("click", function(){
+map_close.addEventListener("click", function() {
   bigmap.classList.remove("popup-show");
   // console.log("Клик по карте");
 });
@@ -15,11 +15,66 @@ map_close.addEventListener("click", function(){
 var feedback_open = document.querySelector(".write-us");
 var bigform = document.querySelector(".feedback-popup-structure");
 var feedback_close = bigform.querySelector(".close-button");
-feedback_open.addEventListener("click", function(evt){
+var username = bigform.querySelector("[name=name]");
+var usermail = bigform.querySelector("[name=email]");
+var userletter = bigform.querySelector("[name=letter]");
+var form = bigform.querySelector("form");
+
+var username_storage = localStorage.getItem("username");
+var usermail_storage = localStorage.getItem("usermail");
+
+
+feedback_open.addEventListener("click", function(evt) {
   evt.preventDefault();
   bigform.classList.add("popup-show");
+  username.focus();
+  if (username_storage) {
+    username.value = username_storage;
+    usermail.focus();
+  }
+  if (usermail_storage) {
+    usermail.value = usermail_storage;
+    userletter.focus();
+  }
 // console.log("Клик по кнопке");
 });
-feedback_close.addEventListener("click", function(){
+form.addEventListener("submit", function(evt) {
+  if (!username.value) {
+    evt.preventDefault();
+    username.classList.add("input-invalid");
+  } else {
+    localStorage.setItem("username", username.value);
+  }
+  if (!usermail.value) {
+    evt.preventDefault();
+    usermail.classList.add("input-invalid");
+  } else {
+    localStorage.setItem("usermail", usermail.value);
+  }
+  if (!userletter.value) {
+    evt.preventDefault();
+    userletter.classList.add("input-invalid");
+  }
+});
+username.addEventListener("focus", function() {
+  username.classList.remove("input-invalid");
+});
+usermail.addEventListener("focus", function() {
+  usermail.classList.remove("input-invalid");
+});
+userletter.addEventListener("focus", function() {
+  userletter.classList.remove("input-invalid");
+});
+feedback_close.addEventListener("click", function() {
   bigform.classList.remove("popup-show");
+});
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode == 27) {
+    if (bigform.classList.contains("popup-show")) {
+      bigform.classList.remove("popup-show");
+    }
+    if (bigmap.classList.contains("popup-show")) {
+      bigmap.classList.remove("popup-show");
+    }
+  }
 });
